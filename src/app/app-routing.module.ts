b44814bd058fee3da547437b830d5612c17e2176
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-09-02 18:02:42 +0300

    refactor: routes changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { StockService } from './services/stock/stock.service';
 import { StockComponent } from './stock/stock.component';
 
 const routes: Routes = [
-  { path: 'stocks', component: StockComponent },
-  { path: '/stock/:isinCode', component: StockComponent }
+  {path: '', component: HomeComponent},
+  { path: 'stock/:isinCode', component: StockComponent }
 ];
 
 @NgModule({
